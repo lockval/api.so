50f532178908a5c1118f8eb0d1410fd4897f7d2c
vanishs <vanishs@hotmail.com>
2023-02-04 01:24:20 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ $(go env GOVERSION) != go1.19.4 ]]; then
+    echo Requires: go1.19.4, currently: $(go env GOVERSION)
+    exit 1
+fi
 
-go build -trimpath -buildmode=plugin -o api.so .
+go build -trimpath -buildmode=plugin -o api.lockval.so .
